8d70eaa008f024750aa4ba62c786e4d714fe83fa
Gorkem Agac <gorkem_agac@hotmail.com>
2022-11-30 11:25:23 +0000

    Hello World

---
+++ src/practice/Test2.java
@@ -0,0 +1,45 @@
+package practice;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test2 {
+
+
+    public static void main(String[] args) {
+       String [] cities = {"Boston","Houston","Austin","Lincoln","Tulsa"};
+       ArrayList<String> list = new ArrayList(Arrays.asList(cities));
+
+       int a = list.size();
+
+       for (String str : list)
+       {
+           String rev = "";
+
+           for (int i = str.length()-1; i >=0 ; i--) {
+               rev+= str.charAt(i);
+
+           }
+           list.set(--a,rev);
+
+       }
+        System.out.println(list);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
