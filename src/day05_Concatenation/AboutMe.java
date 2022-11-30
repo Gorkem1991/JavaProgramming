b0a893cb8f1cabe4ae882dc21466e50e50f28c58
Gorkem Agac <gorkem_agac@hotmail.com>
2022-11-30 11:03:24 +0000

    Hello World

---
+++ src/day05_Concatenation/AboutMe.java
@@ -0,0 +1,9 @@
+package day05_Concatenation;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        String name = "Görkem";
+        int age = 31;
+        System.out.println("Hi my name is " + name + ". I am "+ age+" years old.");
+    }
+}
