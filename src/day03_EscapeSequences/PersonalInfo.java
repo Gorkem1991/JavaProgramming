ceeecaeab49533b2df940d6068a6d0456bdd5d01
Gorkem Agac <gorkem_agac@hotmail.com>
2022-12-23 11:12:49 +0000

    Day 3 Classes

---
+++ src/day03_EscapeSequences/PersonalInfo.java
@@ -0,0 +1,12 @@
+package day03_EscapeSequences;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+        System.out.println("Görkem");
+        System.out.println("Male");
+        System.out.println("19/08/1991");
+        System.out.println("Rua Fernando mauricio");
+        System.out.println("939129206");
+        System.out.println("294039031");
+    }
+}
