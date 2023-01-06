f3bef1f5b7a9e7e63d445f1454996dfaedef97f8
Gorkem Agac <gorkem_agac@hotmail.com>
2023-01-06 23:58:52 +0000

    Day 6 Classes

---
+++ src/day06_PrimitiveTypeCasting/PrimitiveCastings.java
@@ -0,0 +1,30 @@
+package day06_PrimitiveTypeCasting;
+
+public class PrimitiveCastings {
+    public static void main(String[] args) {
+        byte a = 11;
+        short b = (short)a;
+
+        double af = 2.3;
+
+        int v = (int)af;
+
+        double l = 2.3;
+        int m = (int)l;
+
+        double dab = 2.5;
+        short newdab = (short)dab;
+
+        int d = 100;
+        double ff = (double)d/3;
+
+        char star = 9734;
+        char star2 = 8471;
+
+        String starstr = Character.toString(star);
+        String star2str = Character.toString(star2);
+
+        System.out.println(starstr+star2str);
+
+    }
+}
