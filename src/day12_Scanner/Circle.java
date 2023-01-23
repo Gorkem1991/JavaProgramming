21046275550723c0f5a62db5a895f84ff3dc844a
Gorkem Agac <gorkem_agac@hotmail.com>
2023-01-23 17:40:35 +0000

    Day 12 Classes

---
+++ src/day12_Scanner/Circle.java
@@ -0,0 +1,19 @@
+package day12_Scanner;
+
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the radius");
+        double radius = input.nextDouble();
+
+        double area = radius* radius * 3.14;
+        double perimeter = 2 * radius * 3.14;
+
+        System.out.println("area =" + area);
+        System.out.println("perimeter = " + perimeter);
+        input.close();
+    }
+}
