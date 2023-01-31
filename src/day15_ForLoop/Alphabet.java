998f28a99ea26701461b0a556ad551ed8bdd8445
Gorkem Agac <gorkem_agac@hotmail.com>
2023-01-31 09:06:22 +0000

    Day 15 Classes

---
+++ src/day15_ForLoop/Alphabet.java
@@ -0,0 +1,20 @@
+package day15_ForLoop;
+
+public class Alphabet {
+    public static void main(String[] args) {
+
+        for ( char a = 'a'; a<='z'; a++)
+        {
+            System.out.print(a + " ");
+        }
+        for ( char a = 'Z'; a>='A'; a--)
+        {
+            System.out.print(a + " ");
+        }
+        for ( char a = 1; a<=300; a++)
+        {
+            System.out.print(a + " ");
+        }
+
+    }
+}
