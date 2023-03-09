fab249149eb7bf0362f6ae413dadc74f4f2380ea
Gorkem Agac <gorkem_agac@hotmail.com>
2023-03-09 08:38:46 +0000

    Day 26 Classes

---
+++ src/day26_CustomMethodsPractice/ReverseArray.java
@@ -0,0 +1,13 @@
+package day26_CustomMethodsPractice;
+
+import utilities.ArraysUtility;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] numbers = {1,2,3,4,5};
+
+        System.out.println(Arrays.toString(ArraysUtility.reverse(numbers)));
+    }
+}
