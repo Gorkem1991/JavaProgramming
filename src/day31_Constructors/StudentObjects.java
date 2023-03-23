37ce0d870d68ff8f9ec827cf48069bb1f692b6af
Gorkem Agac <gorkem_agac@hotmail.com>
2023-03-23 09:29:07 +0000

    Day 31 Classes

---
+++ src/day31_Constructors/StudentObjects.java
@@ -0,0 +1,13 @@
+package day31_Constructors;
+
+public class StudentObjects {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Gorkem",31, 'M','A',4646);
+        Student student2 = new Student("Hülya",31, 'M','A',4646);
+
+        System.out.println(student1);
+        System.out.println(student2);
+
+    }
+}
