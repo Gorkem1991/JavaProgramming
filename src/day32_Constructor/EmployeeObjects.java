400900bbf7e12d591968fac7c67a7d218f3464ac
Gorkem Agac <gorkem_agac@hotmail.com>
2023-03-27 11:19:53 +0100

    Day 32 Classes

---
+++ src/day32_Constructor/EmployeeObjects.java
@@ -0,0 +1,12 @@
+package day32_Constructor;
+
+public class EmployeeObjects {
+    Employee employee1 = new Employee("Aaron");
+    Employee employee2 = new Employee("Aaron",'F');
+    Employee employee3 = new Employee("Olga",'F',"SDET");
+
+
+
+
+
+}
