fa17d5bcd3ba81c01d95a2d3b35cd7050c22e79f
Gorkem Agac <gorkem_agac@hotmail.com>
2023-03-28 09:41:25 +0100

    Day 33 Classes

---
+++ src/day33_Statics/EmployeeObjects.java
@@ -0,0 +1,13 @@
+package day33_Statics;
+
+public class EmployeeObjects {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        employee1.name = "Ahmet";
+        Employee employee2 = new Employee();
+        employee2.name = "Aygün";
+        Employee employee3 = new Employee();
+        employee3.name = "Fady";
+
+    }
+}
