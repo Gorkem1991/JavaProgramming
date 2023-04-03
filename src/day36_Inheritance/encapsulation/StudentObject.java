2a1740b4cc61d09c3df50b6cd4997be858b81c71
Gorkem Agac <gorkem_agac@hotmail.com>
2023-04-03 05:46:30 +0100

    Day 36 Classes

---
+++ src/day36_Inheritance/encapsulation/StudentObject.java
@@ -0,0 +1,13 @@
+package day36_Inheritance.encapsulation;
+
+import day36_Inheritance.encapsulation.Student;
+
+public class StudentObject {
+    public static void main(String[] args) {
+        Student student1 = new Student("Gorkem",31,'E','A',"Cydeo");
+
+        System.out.println(student1);
+
+    }
+
+}
