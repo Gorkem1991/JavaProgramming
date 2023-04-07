475f9385e06d399c4f84cf385e93c45681b68bb6
Gorkem Agac <gorkem_agac@hotmail.com>
2023-04-08 00:17:24 +0100

    Day 40 Classes

---
+++ src/day40_FinalKeyword/Zoo.java
@@ -0,0 +1,14 @@
+package day40_FinalKeyword;
+
+public class Zoo {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Max","Husky",'M',"White","Large",4);
+
+        System.out.println(dog.getName());
+        System.out.println(dog.getAge());
+
+
+        dog.drink();
+    }
+}
