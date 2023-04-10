e2be6616cef2ec32bb7f5155397e81f91115c70e
Gorkem Agac <gorkem_agac@hotmail.com>
2023-04-10 11:39:54 +0100

    Day 41 Classes

---
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -0,0 +1,30 @@
+package day41_Exceptions;
+
+public class Checked_vs_Unchecked {
+    public static void main(String[] args) {
+
+        //unchecked exception:
+
+        int a =10;
+        int b = 0;
+
+       // System.out.println(a/b);
+        // System.out.println("Wooden Spoon");
+
+       /* char[] characters ={'A','B','C'};
+                            //0,1,0
+        System.out.println(characters[99]);*/
+
+
+        //checked exception
+
+        System.out.println("Hello");
+        //Thread.sleep(3000);
+        System.out.println("Cydeo");
+
+
+
+
+
+    }
+}
