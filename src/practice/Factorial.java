d1f59e8737759bad628db42d3d5b25029fa43970
Gorkem Agac <gorkem_agac@hotmail.com>
2023-06-06 05:29:48 +0100

    practice

---
+++ src/practice/Factorial.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int factorial = sc.nextInt();
+
+        int result = 1;
+        for (int i = 1; i<=factorial; i++)
+        {
+            result = result*i;
+        }
+        System.out.println(result);
+
+    }
+}
