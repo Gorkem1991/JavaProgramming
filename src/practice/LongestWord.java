ed565ec1e4bb6a83f9345f0fe0414a4b37f62dab
Gorkem Agac <gorkem_agac@hotmail.com>
2023-06-18 08:48:38 +0100

    practice

---
+++ src/practice/LongestWord.java
@@ -0,0 +1,29 @@
+package practice;
+import java.util.*;
+import java.io.*;
+
+class LongestWord {
+
+    public static String LongestWord(String sen) {
+
+        String newString = " ";
+
+        for(int i =0; i<sen.length(); i++)
+        {
+            char a =sen.charAt(i);
+
+        }
+
+
+
+        // code goes here
+        return newString;
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(LongestWord(s.nextLine()));
+    }
+
+}
