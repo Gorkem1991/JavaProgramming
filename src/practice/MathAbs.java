3681d4311d0f475046a3f32496817fbd4fa236c5
Gorkem Agac <gorkem_agac@hotmail.com>
2023-07-27 21:56:12 +0100

    practice

---
+++ src/practice/MathAbs.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Scanner;
+
+public class MathAbs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double n = 7.25;
+
+        double m = Math.abs(n);
+        System.out.println(m);
+    }
+}
